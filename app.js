5d447eb06c93cb5e631d55aa689d2f398d34a78a
David Malaver <dmalaver13@gmail.com>
2025-02-05 20:42:12 -0500

    Created functions asignarTestoElemento, validarNombre and agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Declaración de variables
+    let amigos = [];
+  
+    // Función para asignar texto y estilos a los elementos
+    function asignarTextoElemento(elemento, texto, tamaño, color) {
+      let elementoHTML = document.getElementById(elemento);
+      if (elementoHTML) {
+        elementoHTML.innerHTML = texto;
+        elementoHTML.style.fontSize = tamaño;
+        elementoHTML.style.color = color;
+      } else {
+        console.error(`No se encontró el elemento: ${elemento}`);
+      }
+    }
+  
+    // Validar si ingresó un nombre válido
+    function validarNombre() {
+      let nombre = document.getElementById("amigo").value;
+      
+      // Constante de solo letras del alfabeto
+      const regex = /^[A-Za-z]+$/;
+  
+      // Validar si el nombre contiene solo letras
+      if (!regex.test(nombre)) {
+        // Mensaje de error
+        asignarTextoElemento("mensaje", "Nombre ingresado no válido!", "16px", "#d01111");
+        return false; // Nombre no válido
+      }
+      return true; // Nombre válido
+    }
+  
+    // Función para agregar amigo
+    function agregarAmigo() {
+      let nombreAmigo = document.getElementById("amigo").value;
+  
+      // Agregar el nombre si es válido
+      if (validarNombre()) {
+        amigos.push(nombreAmigo);
+        console.log("Amigos:", amigos); // Mostrar la lista de amigos en la consola
+      }
+    }
+  });
+  
